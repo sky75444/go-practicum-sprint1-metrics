c0d16d44b865c13554ac656a2a130b03ab50e65c
ShakirovNA <ShakirovNA@energosf.ru>
2025-07-03 13:16:39 +1000

    Попытка починить

--- internal/handler/updateCounter.go
+++ internal/handler/updateCounter.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -22,58 +21,41 @@ func NewUpdateCounterHandler(umService service.UpdateMetricsService) *UpdateCoun
 func (c *UpdateCounterHandler) Handle() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
 		if r.Header.Get("Content-Type") != "text/plain" {
-			http.Error(w, "Content-Type not allowed", http.StatusMethodNotAllowed)
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			// http.Error(w, "Content-Type not allowed", http.StatusMethodNotAllowed)
 			return
 		}
 
 		metricValueStr := r.URL.Path[strings.LastIndex(r.URL.Path, "/")+1:]
 		urlNameValue := r.URL.Path[16:]
 		metricName := urlNameValue[:strings.LastIndex(r.URL.Path, "/")]
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("МОЙ ВЫВОД")
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("r.URL.Path")
-		fmt.Println(r.URL.Path)
-		fmt.Println("metricValueStr")
-		fmt.Println(metricValueStr)
-		fmt.Println("urlNameValue")
-		fmt.Println(urlNameValue)
-		fmt.Println("metricName")
-		fmt.Println(metricName)
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("МОЙ ВЫВОД")
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
-		fmt.Println("********************************************")
 
 		if metricName == "" {
-			http.Error(w, "metric name is required", http.StatusNotFound)
+			w.WriteHeader(http.StatusNotFound)
+			// http.Error(w, "metric name is required", http.StatusNotFound)
 			return
 		}
 
 		value, err := strconv.ParseInt(metricValueStr, 10, 64)
 		if err != nil {
-			http.Error(w, "invalid counter value", http.StatusBadRequest)
+			w.WriteHeader(http.StatusBadRequest)
+			// http.Error(w, "invalid counter value", http.StatusBadRequest)
 			return
 		}
 
 		if err := c.updateMetricsService.UpdateCounter(metricName, value); err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			w.WriteHeader(http.StatusInternalServerError)
+			// http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
 
-		// w.WriteHeader(http.StatusOK)
-		fmt.Fprint(w)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Metric updated"))
 	}
 }
