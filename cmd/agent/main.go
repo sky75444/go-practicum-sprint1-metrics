2f683d316ea2d631647049c08f2279e709f2609e
ShakirovNA <ShakirovNA@energosf.ru>
2025-07-03 11:50:54 +1000

    Код первого инкремента должен быть в агенте а не в сервере чтоли. Судя по документации к автотестам

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sky75444/go-practicum-sprint1-metrics/internal/app"
+)
+
+func main() {
+	d := app.NewDI()
+	d.Init()
+
+	fmt.Println("Server started at http://localhost:8080")
+
+	if err := http.ListenAndServe(`:8080`, d.Router.Mux); err != nil {
+		panic(err)
+	}
+}
