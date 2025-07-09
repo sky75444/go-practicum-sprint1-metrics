c0f8c6e3799e30b92c37c16170f6237949604372
ShakirovNA <ShakirovNA@energosf.ru>
2025-07-09 12:01:12 +1000

    Добавил протокол в хост если он не указан

--- cmd/agent/agentflags/agentflags.go
+++ cmd/agent/agentflags/agentflags.go
@@ -22,6 +22,9 @@ func NewParsedFlags() *flags {
 		//Если длина 5, это значит что хост не указан. А для агента важно знать хост
 		flags.memServerAddr = fmt.Sprintf("http://localhost%s", flags.memServerAddr)
 	}
+	if flags.memServerAddr[:4] != "http:" {
+		flags.memServerAddr = fmt.Sprintf("http://%s", flags.memServerAddr)
+	}
 	return &flags
 }
 
