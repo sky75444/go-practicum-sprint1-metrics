6059d1d20bb91dcf37cf0329b4ca2172d6004673
ShakirovNA <ShakirovNA@energosf.ru>
2025-07-28 17:21:51 +1000

    Добавил отладочные сообщения

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/sky75444/go-practicum-sprint1-metrics/cmd/agent/agentflags"
 	"github.com/sky75444/go-practicum-sprint1-metrics/internal/agent/agentconfig"
 	"github.com/sky75444/go-practicum-sprint1-metrics/internal/agent/app"
@@ -16,6 +18,9 @@ func main() {
 	di := app.NewDI()
 	di.Init(c)
 
+	fmt.Printf("Start endless collect metrics on: %s\n", c.MemStorageServerAddr)
+	fmt.Printf("pollInterval: %d\n", c.PollInterval)
+	fmt.Printf("reportInterval: %d\n", c.ReportInterval)
 	if err := di.Services.MetricCollectorAgent.EndlessCollectMetrics(di.Client); err != nil {
 		panic(err)
 	}
