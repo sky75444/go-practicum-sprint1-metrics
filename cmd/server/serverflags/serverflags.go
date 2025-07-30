e6a91b37b6159f824925fae91a8f536ab78bf5d3
ShakirovNA <ShakirovNA@energosf.ru>
2025-07-30 10:33:49 +1000

    Не занметил в задании что надо и сервер тоже изменить

--- cmd/server/serverflags/serverflags.go
+++ cmd/server/serverflags/serverflags.go
@@ -1,15 +1,33 @@
 package serverflags
 
-import "flag"
+import (
+	"flag"
+	"log"
+
+	"github.com/caarlos0/env"
+)
 
 type flags struct {
 	runAddr string
+}
+
+type envFlags struct {
+	Address string `env:"ADDRESS"`
 }
 
 func NewParsedFlags() *flags {
 	flags := flags{}
 	flag.StringVar(&flags.runAddr, "a", ":8080", "address and port to run server")
 	flag.Parse()
+
+	var ef envFlags
+	if err := env.Parse(&ef); err != nil {
+		log.Fatal(err)
+	}
+
+	if ef.Address != "" {
+		flags.runAddr = ef.Address
+	}
 
 	return &flags
 }
